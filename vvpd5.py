7dd9799b13a7b45a3692c8a39003b5e220afc284
EkaterinaSap <sapchenkoekaterina@gmail.com>
2024-12-17 21:20:14 +0700

    Вторая функция программы

--- vvpd5.py
+++ vvpd5.py
@@ -14,6 +14,20 @@ def maclaurin_ln1_plus_x(x, iterations=4):
     return result
 
 
+def maclaurin_ln_1_minus_x(x, iterations=4):
+    """
+    Вычисляет приближенное значение ln(1 - x) с использованием разложения в ряд Маклорена
+    :param x: значение, для которого вычисляется ln(1 - x) в пределе (-1, 1)
+    :param iterations: количество итераций (по умолчанию 4)
+    :return: приближенное значение ln(1 - x)
+    """
+    if not (-1 < x < 1):
+        raise ValueError("Значение x должно быть в диапазоне (-1, 1).")
+    result = 0
+    for n in range(1, iterations + 1):
+        maclaurin = (x ** n) / n
+        result += maclaurin
+    return -result
 
 
 
